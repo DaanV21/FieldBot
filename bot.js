10d5f93b874da6a40c8fab9d2e098b217b031e14
DaanV21 <50521771+DaanV21@users.noreply.github.com>
2019-05-12 21:11:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,24 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
    
-    client.on('message', msg=>{
-    if(msg.content === '/info'){
-        msg.reply('**Er is momenteel geen informatie beschikbaar.**');
+client.on('message', message=>{
+   
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+switch(args[0]){
+case 'info':
+message.channel.sendMessage('**Er is momenteel geen informatie beschikbaar, probeer het later opnieuw.**')
+break;
+case 'website':
+message.channel.sendMessage('**Dit commando is momenteel niet in gebruik.**')
+    break;
+case 'serverstatus':
+message.channel.sendMessage('**De server is momenteel **')
+    break;
+case 'clear':
+    if(!args[1]) return message.reply('Error: Geef aan hoeveel berichten er moeten worden verwijderd.')
+    message.channel.bulkDelete(args[1]);
+    break;
     }
 });
     
